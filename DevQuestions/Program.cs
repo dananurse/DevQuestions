1af69c61b2115ef29f63a9a067b0517755e71689
dana.nurse <dana.nurse@outlook.com>
2015-02-20 22:54:20 +0000

    Initial Commit of Code

---
+++ DevQuestions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DevQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass bc = new BaseClass();
+            InheritingClass1 ic1 = new InheritingClass1();
+            InheritingClass2 ic2 = new InheritingClass2();
+
+
+            Console.WriteLine(bc.GetString());
+            Console.WriteLine(ic1.GetString());
+            Console.WriteLine(ic2.GetString());
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+public class BaseClass
+{
+    public virtual string GetString()
+    {
+        return "Value from base class";
+    }
+}
+
+public class InheritingClass1 : BaseClass
+{
+    public override string GetString()
+    {
+        return "Value from inheriting class";
+    }
+}
+
+public class InheritingClass2 : BaseClass
+{
+}
+
